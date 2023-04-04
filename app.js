7e974071fe84063330c9c39ffdadb756c02cd179
Alex Rankin <alexrankinpartner@gmail.com>
2023-04-04 22:34:46 +1200

    Prismic Intergration

--- app.js
+++ app.js
@@ -1,17 +1,141 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch')
 const express = require('express')
-const app = express()
 const path = require('path')
+const errorHandler = require('errorhandler')
+
+const app = express()
 const port = 3000
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+const Prismic = require('@prismicio/client')
+const PrismicH = require('@prismicio/helpers')
 
-app.get('/', (reg, res) => {
-  res.render('pages/home')
+const { application } = require('express')
+const UAParser = require('ua-parser-js')
+
+app.use(errorHandler())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch
+  })
+}
+
+// Link Resolver
+const HandleLinkResolver = (doc) => {
+  // Default to homepage
+  return '/'
+}
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  const ua = UAParser(req.headers['user-agent'])
+
+  res.locals.isDesktop = ua.device.type === undefined
+  res.locals.isPhone = ua.device.type === 'mobile'
+  res.locals.isTablet = ua.device.type === 'tablet'
+
+  res.locals.Link = HandleLinkResolver
+  res.locals.PrismicH = PrismicH
+  res.locals.Numbers = (index) => {
+    return index === 0
+      ? 'One'
+      : index === 1
+        ? 'Two'
+        : index === 2
+          ? 'Three'
+          : index === 3
+            ? 'Four'
+            : ''
+  }
+  next()
 })
 
-app.get('/about', (reg, res) => {
-  res.render('pages/about')
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.locals.basedir = app.get('views')
+
+const handleRequest = async (api) => {
+  const [meta, preloader, navigation, home, about, { results: collections }] =
+    await Promise.all([
+      api.getSingle('meta'),
+      api.getSingle('preloader'),
+      api.getSingle('navigation'),
+      api.getSingle('home'),
+      api.getSingle('about'),
+      api.query(Prismic.Predicates.at('document.type', 'collection'), {
+        fetchLinks: 'product.image'
+      })
+    ])
+
+  //   console.log(about, home, collections);
+
+  const assets = []
+
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url)
+      })
+    }
+  })
+
+  collections.forEach((collection) => {
+    collection.data.list.forEach((item) => {
+      assets.push(item.product.data.image.url)
+    })
+  })
+
+  console.log(assets)
+
+  return {
+    assets,
+    meta,
+    home,
+    collections,
+    about,
+    navigation,
+    preloader
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/home', {
+    ...defaults,
+  })
+})
+
+app.get('/projects', async (req, res) => {
+  res.render('pages/projects')
+})
+
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults,
+  })
+})
+
+app.get('/contact', async (req, res) => {
+  res.render('pages/contact')
 })
 
 app.listen(port, () => {
